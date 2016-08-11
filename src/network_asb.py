9ffe2f13494c4f4806c59702f9b9468d7f22f741
akhilsbehl <akhilsbehl@gmail.com>
2016-08-11 22:32:43 +0530

    Adding a matrix version of backprop.

---
+++ src/network_asb.py
@@ -0,0 +1,135 @@
+"""
+network.py
+~~~~~~~~~~
+
+A module to implement the stochastic gradient descent learning
+algorithm for a feedforward neural network.  Gradients are calculated
+using backpropagation.  Note that I have focused on making the code
+simple, easily readable, and easily modifiable.  It is not optimized,
+and omits many desirable features.
+"""
+
+import random
+import numpy as np
+
+class Network(object):
+
+    def __init__(self, sizes):
+        """The list ``sizes`` contains the number of neurons in the
+        respective layers of the network.  For example, if the list
+        was [2, 3, 1] then it would be a three-layer network, with the
+        first layer containing 2 neurons, the second layer 3 neurons,
+        and the third layer 1 neuron.  The biases and weights for the
+        network are initialized randomly, using a Gaussian
+        distribution with mean 0, and variance 1.  Note that the first
+        layer is assumed to be an input layer, and by convention we
+        won't set any biases for those neurons, since biases are only
+        ever used in computing the outputs from later layers."""
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        """Return the output of the network if ``a`` is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+            test_data=None):
+        """Train the neural network using mini-batch stochastic
+        gradient descent.  The ``training_data`` is a list of tuples
+        ``(x, y)`` representing the training inputs and the desired
+        outputs.  The other non-optional parameters are
+        self-explanatory.  If ``test_data`` is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out.  This is useful for
+        tracking progress, but slows things down substantially."""
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print "Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test)
+            else:
+                print "Epoch {0} complete".format(j)
+
+    def update_mini_batch(self, mini_batch, eta):
+        """Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
+        is the learning rate."""
+        nabla_b, nabla_w = self.backprop(mini_batch)
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, mini_batch):
+        """Return a vector of ``(nabla_b, nabla_w)`` representing the
+        gradient for the cost function C_x.  ``nabla_b`` and
+        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+        to ``self.biases`` and ``self.weights``."""
+        nabla_b = [None for b in self.biases]
+        nabla_w = [None for w in self.weights]
+        activation, output = [np.array(x) for x in zip(*mini_batch)]
+        activation = activation[:, :, 0].T
+        output = output[:, :, 0].T
+        activations, zs = [activation], []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_derivative(activations[-1], output)
+        delta *= sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta.sum(axis=1)[:, np.newaxis]
+        nabla_w[-1] = self._compute_nabla_w(delta, activations[-2])
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            delta = np.dot(self.weights[-l + 1].transpose(), delta)
+            delta *= sigmoid_prime(z)
+            nabla_b[-l] = delta.sum(axis=1)[:, np.newaxis]
+            nabla_w[-l] = self._compute_nabla_w(delta, activations[-l - 1])
+        return (nabla_b, nabla_w)
+
+    @staticmethod
+    def _compute_nabla_w(delta, activation):
+        tmp = [np.dot(delta[:, i, np.newaxis],
+                      activation[:, i, np.newaxis].T) for
+               i in xrange(delta.shape[1])]
+        nabla_w = reduce(np.add, tmp)
+        return nabla_w
+
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """Return the vector of partial derivatives \partial C_x /
+        \partial a for the output activations."""
+        return (output_activations - y)
+
+
+def sigmoid(z):
+    """The sigmoid function."""
+    ez = np.exp(-z)
+    return 1.0 / (1.0 + ez)
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    sz = sigmoid(z)
+    return sz * (1 - sz)
